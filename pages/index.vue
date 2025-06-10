1b48450a3d88967589cd1e0ea852eb0e13b12eca
Howard <hw@yuanhau.com>
2025-06-10 17:10:24 +0800

    Borders & Apple's Lquid Glass Effect.

--- pages/index.vue
+++ pages/index.vue
@@ -65,9 +65,9 @@ onMounted(async () => {
     class="justify-center align-center text-center selection:opactiy-[50%] absolute inset-0 flex flex-col p-10 md:p-20 lg:p-40 xl:p-60 2xl:p-80"
   >
     <div
-      class="flex flex-col justify-center items-center bg-gray-300/50 backdrop-blur-sm z-10 p-3 rounded-lg shadow-lg py-10"
+      class="flex flex-col justify-center items-center bg-gray-300/5 backdrop-blur-sm z-10 p-3 rounded-lg shadow-lg py-10 border-2 border-gray-400/40"
     >
-      <h1 class="text-xl sm:text-2xl md:text-4xl font-bold text-white mb-4">
+      <h1 class="text-xl sm:text-2xl md:text-4xl font-bold text-white mb-4 text-border">
         AIOT 生態物種即時監測回報裝置網頁系統
       </h1>
       <p class="text-lg text-white mb-8">歡迎使用管理系統</p>
