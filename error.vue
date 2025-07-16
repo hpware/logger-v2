1f06e3966a73da4c347e7c6c442151e8c26d34a4
Howard <hw@yuanhau.com>
2025-07-16 10:15:04 +0800

    feat: Enhance error handling and authorization checks in createDatabase route

--- error.vue
+++ error.vue
@@ -1,9 +1,16 @@
+<script lang="ts" setup>
+useSeoMeta({
+  title: "404 Not Found",
+  description: "對不起，這個的頁面不存在或已被移除。",
+});
+const error = useError();
+</script>
 <template>
   <div
     class="text-center justify-center align-center absolute inset-0 flex flex-col"
   >
-    <div class="text-6xl font-bold">404 Not Found</div>
-    <div class="text-2xl">對不起，這個的頁面不存在或已被移除。</div>
+    <div class="text-6xl font-bold">{{ error.statusCode }} {{ error.statusMessage }}</div>
+    <div class="text-2xl">{{error.statusCode === 404 ? "對不起，這個的頁面不存在或已被移除。" : error.message}}</div>
     <div class="text-lg">如果您認為這是錯誤，請聯繫網站管理員。</div>
   </div>
 </template>
