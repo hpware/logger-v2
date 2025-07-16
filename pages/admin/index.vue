ad3a094c0b8a74a6382b07343442153f9c1a75f4
Howard <hw@yuanhau.com>
2025-07-16 10:24:08 +0800

    Uhhhh Vercel? WHERE IS MY DEPLOYMENT?

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -18,21 +18,21 @@ useSeoMeta({
           class="group block p-6 bg-blue-500 rounded-lg hover:bg-blue-600 transition-all duration-300 ease-in-out transform hover:-translate-y-1"
         >
           <h2 class="text-xl font-semibold text-white">加入新機器</h2>
-          <p class="text-blue-200 mt-2">新增並設定新的裝置</p>
+          <p class="text-blue-200 mt-2">新增裝置</p>
         </NuxtLink>
         <NuxtLink
           to="/admin/deletemachines"
           class="group block p-6 bg-red-500 rounded-lg hover:bg-red-600 transition-all duration-300 ease-in-out transform hover:-translate-y-1"
         >
           <h2 class="text-xl font-semibold text-white">刪除舊機器</h2>
-          <p class="text-red-200 mt-2">移除不再使用的裝置</p>
+          <p class="text-red-200 mt-2">移除裝置</p>
         </NuxtLink>
         <a
           href="/createDatabase"
           class="group block p-6 bg-green-500 rounded-lg hover:bg-green-600 transition-all duration-300 ease-in-out transform hover:-translate-y-1"
         >
           <h2 class="text-xl font-semibold text-white">創立資料庫</h2>
-          <p class="text-green-200 mt-2">初始化或重設資料庫</p>
+          <p class="text-green-200 mt-2">初始化資料庫</p>
         </a>
       </div>
     </div>
