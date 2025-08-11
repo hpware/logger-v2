fa5a17f4f0e859d49d9d2574feedc76451397b17
Howard <hw@yuanhau.com>
2025-08-11 10:41:20 +0800

    Add custom favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,4 +14,20 @@ export default defineNuxtConfig({
         process.env.NUXT_PUBLIC_BETTER_AUTH_URL || "http://localhost:3000",
     },
   },
+  app: {
+    head: {
+      title: "AIOT 生態物種即時監測回報裝置網頁系統",
+      htmlAttrs: {
+        lang: "zh-Hant",
+      },
+      link: [
+        { rel: "icon", type: "image/x-icon", href: "/favicon.svg" },
+      ],
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        { "http-equiv": "X-UA-Compatible", content: "IE=edge" },
+      ]
+    } 
+    }
 });
